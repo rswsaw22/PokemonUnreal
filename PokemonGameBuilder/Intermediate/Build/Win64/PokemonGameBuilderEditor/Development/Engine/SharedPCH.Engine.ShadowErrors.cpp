4e916084140bd450547a76b533b162fad6c16346
Johnathan Becker <historybuffjb@gmail.com>
2021-01-02 14:14:20 -0600

    Changed to godot

--- PokemonGameBuilder/Intermediate/Build/Win64/PokemonGameBuilderEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.28.29335
-#include "C:/Users/histo/Documents/GitHub/PokemonUnreal/PokemonGameBuilder/Intermediate/Build/Win64/PokemonGameBuilderEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- PokemonGameBuilder/Source/PokemonGameBuilder/PokemonGameBuilderGameModeBase.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
-#include "PokemonGameBuilderGameModeBase.h"
-
